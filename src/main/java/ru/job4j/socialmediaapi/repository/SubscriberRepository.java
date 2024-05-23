fac596ab64dd3c4d3cf74d7e62ac2825816f22cb
origin <git@github.com:OljaKn/job4j_design.git>
2024-05-23 14:47:44 +0300

    Spring Data JPA

--- src/main/java/ru/job4j/socialmediaapi/repository/SubscriberRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/SubscriberRepository.java
@@ -3,5 +3,5 @@ package ru.job4j.socialmediaapi.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.socialmediaapi.model.Subscriber;
 
-public interface SubscriberRepository extends CrudRepository<Subscriber,Long> {
+public interface SubscriberRepository extends CrudRepository<Subscriber, Long> {
 }
