74c3bb9fce3c9ebf72816fc1d7ff9c05a8ea4d30
origin <git@github.com:OljaKn/job4j_design.git>
2024-05-24 13:40:50 +0300

    Именованные запросы

--- src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
+++ src/main/java/ru/job4j/socialmediaapi/repository/PostRepository.java
@@ -3,5 +3,14 @@ package ru.job4j.socialmediaapi.repository;
 import org.springframework.data.repository.CrudRepository;
 import ru.job4j.socialmediaapi.model.Post;
 
+import java.awt.print.Pageable;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface PostRepository extends CrudRepository<Post, Long> {
+    List<Post> findByUserId(int userId);
+
+    List<Post> findByCreatedBetween(LocalDateTime startDate, LocalDateTime finishDate);
+
+    List<Post> findByOrderByCreatedDesc(Pageable pageable);
 }
